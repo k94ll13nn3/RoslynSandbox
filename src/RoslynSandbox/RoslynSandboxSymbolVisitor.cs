2e17155481ef242e6ff44f53eee7bdd1f6f0ffb4
Keuvain <k94ll13nn3@outlook.fr>
2017-08-28 21:00:13 +0200

    Add decomposition of method symbols

--- src/RoslynSandbox/RoslynSandboxSymbolVisitor.cs
+++ src/RoslynSandbox/RoslynSandboxSymbolVisitor.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
+using System.Text;
 using Microsoft.CodeAnalysis;
 
 namespace RoslynSandbox
@@ -15,22 +18,18 @@ namespace RoslynSandbox
 
             if (symbol is INamespaceOrTypeSymbol namespaceOrTypeSymbol)
             {
-                foreach (var child in namespaceOrTypeSymbol.GetMembers())
+                foreach (ISymbol child in namespaceOrTypeSymbol.GetMembers())
                 {
                     child.Accept(this);
                 }
             }
 
             var kind = symbol.Kind.ToString();
-            var implementedSymbol = IsInterfaceImplementation(symbol);
+            (ISymbol symbol, bool isInterfaceImplementation) implementedSymbol = IsInterfaceImplementation(symbol);
             var symbolType = string.Empty;
             if (implementedSymbol.isInterfaceImplementation)
             {
                 symbolType = $"implements {implementedSymbol.symbol}";
-            }
-            else if (symbol.IsOverride && symbol is IMethodSymbol m)
-            {
-                symbolType = $"overrides {m.OverriddenMethod}";
             }
             else if (symbol.IsOverride && symbol is IPropertySymbol p)
             {
@@ -45,9 +44,83 @@ namespace RoslynSandbox
             symbol.GlobalNamespace.Accept(this);
         }
 
+        public override void VisitMethod(IMethodSymbol symbol)
+        {
+            if (symbol.MethodKind != MethodKind.Ordinary)
+            {
+                DefaultVisit(symbol);
+                return;
+            }
+
+            ITypeSymbol returnType = symbol.ReturnType;
+            ImmutableArray<IParameterSymbol> parameters = symbol.Parameters;
+            ImmutableArray<ITypeParameterSymbol> typeParameters = symbol.TypeParameters;
+
+            var kind = symbol.MethodKind.ToString();
+            (ISymbol symbol, bool isInterfaceImplementation) implementedSymbol = IsInterfaceImplementation(symbol);
+            var symbolType = string.Empty;
+            if (implementedSymbol.isInterfaceImplementation)
+            {
+                symbolType = $"implements {implementedSymbol.symbol}";
+            }
+            else if (symbol.IsOverride)
+            {
+                symbolType = $"overrides {symbol.OverriddenMethod}";
+            }
+
+            var info = new StringBuilder();
+            info.Append($"{returnType.Name} ");
+            info.Append($"{symbol.Name}");
+
+            if (typeParameters.Length > 0)
+            {
+                info.Append($"<{string.Join(", ", typeParameters)}>");
+            }
+
+            info.Append("(");
+            info.Append(string.Join(", ", parameters.Select(p => $"{p.Type.Name} { p.Name}")));
+            info.Append(")");
+
+            if (typeParameters.Any(x => HasConstraints(x)))
+            {
+                foreach (ITypeParameterSymbol type in typeParameters.Where(x => HasConstraints(x)))
+                {
+                    // struct or class then herited class then interfaces then new()
+                    info.Append(" where ");
+                    info.Append($"{type.Name} : ");
+
+                    var constraints = new List<string>();
+                    if (type.HasReferenceTypeConstraint)
+                    {
+                        constraints.Add("class");
+                    }
+                    if (type.HasValueTypeConstraint)
+                    {
+                        constraints.Add("struct");
+                    }
+                    constraints.AddRange(type.ConstraintTypes.Select(x => x.Name));
+                    if (type.HasConstructorConstraint)
+                    {
+                        constraints.Add("new()");
+                    }
+
+                    info.Append(string.Join(", ", constraints));
+                }
+            }
+
+            // T Convert<U, V>(U data, string name, V id) where U : Data, new()
+            Console.WriteLine($"{info} ({kind}): {symbolType}");
+        }
+
+        private bool HasConstraints(ITypeParameterSymbol symbol) =>
+            symbol.HasValueTypeConstraint ||
+            symbol.HasConstructorConstraint ||
+            symbol.HasReferenceTypeConstraint ||
+            symbol.ConstraintTypes.Length > 0;
+
         private (ISymbol symbol, bool isInterfaceImplementation) IsInterfaceImplementation<T>(T method) where T : class, ISymbol
         {
-            var symbol = method?
+            T symbol = method?
                 .ContainingType?
                 .AllInterfaces
                 .SelectMany(@interface => @interface
--- src/RoslynTestLibrary/ITestInterface.cs
+++ src/RoslynTestLibrary/ITestInterface.cs
@@ -1,13 +1,13 @@
 ﻿namespace RoslynTestLibrary
 {
-    public interface ITestInterface<out T> where T : Data
+    public interface ITestInterface<out T, T2> where T : Data
     {
         T Get();
 
         T Convert(Data data);
 
-        T Convert<U>(U data) where U : Data;
+        T Convert<U>(U data, T2 t) where U : Data, IData;
 
-        T Convert<U, V>(U data, string name, V id) where U : Data;
+        T Convert<U, V>(U data, string name, V id) where U : Data, new() where V : class;
     }
 }
