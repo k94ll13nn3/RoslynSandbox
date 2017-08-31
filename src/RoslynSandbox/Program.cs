daf5e31e4b20794784dd26eadda7229e4fb5843a
Keuvain <k94ll13nn3@outlook.fr>
2017-08-31 20:30:36 +0200

    Revert project to net47 to avoid problems with mscorlib

--- src/RoslynSandbox/Program.cs
+++ src/RoslynSandbox/Program.cs
@@ -4,10 +4,6 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using RoslynTestLibrary;
 
-// see https://github.com/dotnet/roslyn/issues/6138
-// see https://joshvarty.wordpress.com/2015/10/25/learn-roslyn-now-part-15-the-symbolvisitor/
-// see https://github.com/Wyamio/Wyam/blob/develop/src/extensions/Wyam.CodeAnalysis/Analysis/AnalyzeSymbolVisitor.cs
-// see https://stackoverflow.com/a/42075734/3836163
 namespace RoslynSandbox
 {
     internal class Program
